21531d61b7aaffae0455eb200455a6be55328b82
Pradyut Lohani <pradyut.lohani@gmail.com>
2020-08-13 10:29:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from recommendor import recommendor
+#from recommendor import recommendor
 
 st.set_option("deprecation.showfileUploaderEncoding", False)
 st.markdown('<style>body{background-image: url("https://i.redd.it/4fxxbm4opjd31.jpg"); background-repeat: no-repeat; background-attachment: fixed; background-size: 100% 100%;} body::before{content: ""; position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; background-color: rgba(1,1,1,0.95);}</style>',unsafe_allow_html=True)
@@ -17,10 +17,9 @@ st.sidebar.header("Enter movie name and rating")
 st.sidebar.markdown("")
 
 userLikes=[]
-n=1
-n=st.sidebar.text_input('How many movies do you want to enter?',value=0)
+n=st.sidebar.number_input("How many movies do you want to enter?",value=0)
 i=0
-while i<int(n):
+while i<n:
     movies={}
     movie=st.sidebar.text_input("Movie Title",key=i)
     rating=st.sidebar.slider("Rating",min_value=0.01,max_value=5.01,key=i)
